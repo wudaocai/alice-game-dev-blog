87354e53898397f61be50253fdc3eabce771269d
wudaocai <2497440993@qq.com>
2026-04-25 00:04:06 +0800

    Switch to Netlify native Next.js build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  basePath: '/alice-game-dev-blog',
 }
 
 module.exports = nextConfig
